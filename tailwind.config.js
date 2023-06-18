7ef63b03db25794b4058a94223bbaa489ea3709f
majidamini <majidamini_s@yahoo.com>
2023-06-18 12:19:26 +0330

    php codes removed and styles added

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: "#dff6ff",
+          200: "#b6e8fb",
+          300: "#a4bce6",
+          400: "#22d3ee",
+          500: "#7d94bd",
+          600: "#6889c3",
+          700: "#232dbd",
+          800: "#192089",
+          900: "#10166a",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
